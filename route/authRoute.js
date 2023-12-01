25cb5f2f8b4a09810087957dcd80dd7814d8c6ed
vvenetillo <venetillovictor@gmail.com>
2023-12-01 19:12:34 -0300

    correção de bug

--- route/authRoute.js
+++ route/authRoute.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const User = require('../models/user');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
 
 // Rota para o cadastro de usuários (Sign Up)
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
   try {
     const { nome, email, senha, telefones } = req.body;
 
     // Verifica se o e-mail já está cadastrado
     const existingUser = await User.findOne({ email: email });
     if (existingUser) {
-      return res.status(400).json({ mensagem: 'E-mail já cadastrado' });
+      return res.status(400).json({ mensagem: "E-mail já cadastrado" });
     }
 
     // Hash da senha
@@ -31,7 +31,7 @@ app.post('/signup', async (req, res) => {
 
     // Gera um token JWT
     const token = jwt.sign({ userId: savedUser._id }, process.env.JWT_SECRET, {
-      expiresIn: '30m', // Tempo de expiração do token
+      expiresIn: "30m", // Tempo de expiração do token
     });
 
     res.status(201).json({
@@ -40,30 +40,29 @@ app.post('/signup', async (req, res) => {
       data_atualizacao: savedUser.data_atualizacao,
       ultimo_login: savedUser.ultimo_login,
       token,
-      mensagem: 'Usuário cadastrado com sucesso',
+      mensagem: "Usuário cadastrado com sucesso",
     });
   } catch (error) {
-    console.error('Erro no cadastro de usuário:', error);
-    res.status(500).json({ mensagem: 'Erro interno no servidor' });
+    console.error("Erro no cadastro de usuário:", error);
+    res.status(500).json({ mensagem: "Erro interno no servidor" });
   }
 });
 
-
 // Rota de login
-app.post('/signin', async (req, res) => {
+app.post("/signin", async (req, res) => {
   try {
     const { email, senha } = req.body;
 
     // Verifica se o usuário está cadastrado
     const user = await User.findOne({ email: email });
     if (!user) {
-      return res.status(401).json({ mensagem: 'Usuário não cadastrado' });
+      return res.status(401).json({ mensagem: "Usuário não cadastrado" });
     }
 
     // Verifica a senha
     const isPasswordValid = await user.comparePassword(senha);
     if (!isPasswordValid) {
-      return res.status(401).json({ mensagem: 'Usuário e/ou senha inválidos' });
+      return res.status(401).json({ mensagem: "Usuário e/ou senha inválidos" });
     }
 
     // Atualiza o último login
@@ -72,7 +71,7 @@ app.post('/signin', async (req, res) => {
 
     // Gera um novo token JWT
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30m',
+      expiresIn: "30m",
     });
 
     res.json({
@@ -83,34 +82,38 @@ app.post('/signin', async (req, res) => {
       token,
     });
   } catch (error) {
-    console.error('Erro na autenticação:', error);
-    res.status(500).json({ mensagem: 'Erro interno no servidor', error: error.message });
+    console.error("Erro na autenticação:", error);
+    res
+      .status(500)
+      .json({ mensagem: "Erro interno no servidor", error: error.message });
   }
 });
 
-
 // Middleware para verificar o token de autenticação
 const verifyToken = (req, res, next) => {
-  const token = req.header('Authorization');
+  const token = req.header("Authorization");
   if (!token) {
-    return res.status(401).json({ mensagem: 'Não autorizado' });
+    return res.status(401).json({ mensagem: "Não autorizado" });
   }
 
   try {
-    const decoded = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
+    const decoded = jwt.verify(
+      token.replace("Bearer ", ""),
+      process.env.JWT_SECRET
+    );
     req.userId = decoded.userId;
     next();
   } catch (error) {
-    res.status(401).json({ mensagem: 'Não autorizado' });
+    res.status(401).json({ mensagem: "Não autorizado" });
   }
 };
 
 // Rota para buscar informações do usuário
-app.get('/user', verifyToken, async (req, res) => {
+app.get("/user", verifyToken, async (req, res) => {
   try {
     const user = await User.findById(req.userId);
     if (!user) {
-      return res.status(404).json({ mensagem: 'Usuário não encontrado' });
+      return res.status(404).json({ mensagem: "Usuário não encontrado" });
     }
 
     res.json({
@@ -118,34 +121,33 @@ app.get('/user', verifyToken, async (req, res) => {
       data_criacao: user.data_criacao,
       data_atualizacao: user.data_atualizacao,
       ultimo_login: user.ultimo_login,
-      token: req.header('Authorization'),
+      token: req.header("Authorization"),
     });
   } catch (error) {
-    console.error('Erro ao buscar usuário:', error);
-    res.status(500).json({ mensagem: 'Erro interno no servidor' });
+    console.error("Erro ao buscar usuário:", error);
+    res.status(500).json({ mensagem: "Erro interno no servidor" });
   }
 });
 
 // Private-Route
-app.get('/user/:id', verifyToken, async (req, res) => {
+app.get("/user/:id", verifyToken, async (req, res) => {
   try {
     const user = await User.findById(req.userId);
     if (!user) {
-      return res.status(404).json({ mensagem: 'Usuário não encontrado' });
+      return res.status(404).json({ mensagem: "Usuário não encontrado" });
     }
-    res.status(200).json({ user: user})
+    res.status(200).json({ user: user });
     res.json({
       id: user._id,
       data_criacao: user.data_criacao,
       data_atualizacao: user.data_atualizacao,
       ultimo_login: user.ultimo_login,
-      token: req.header('Authorization'),
+      token: req.header("Authorization"),
     });
   } catch (error) {
-    console.error('Erro ao buscar usuário:', error);
-    res.status(500).json({ mensagem: 'Erro interno no servidor' });
+    console.error("Erro ao buscar usuário:", error);
+    res.status(500).json({ mensagem: "Erro interno no servidor" });
   }
 });
 
-
-module.exports = router;
+module.exports = app;
