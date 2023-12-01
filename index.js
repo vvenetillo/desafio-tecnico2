72a8f9d0627960c2a235dba1266fe4b83ba49b83
vvenetillo <venetillovictor@gmail.com>
2023-12-01 19:34:16 -0300

    update dev dependencies  e index

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyparser = require("body-parser");
 const authRoutes = require("./route/authRoute");
+const cors = require("cors");
+
 
 const app = express();
-app.use(bodyparser.json());
+app.use(express.json()); // Use o middleware nativo do Express para análise JSON
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET, POST",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const port = process.env.PORT || 3001;
+
+app.use(authRoutes); // Configurar middleware de rotas
 
 mongoose
   .connect(
     `mongodb+srv://${dbUser}:${dbPassword}@auth.ypun52w.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(port, () => {
-      console.log(`Servidor rodando na port ${port}`);
-      app.use( authRoutes); 
-      app.use((err, req, res, next) => {
-        if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
-          return res.status(400).json({ mensagem: "Erro de análise JSON" });
-        }
-        next();
-      });
-    });
     console.log("Conectou ao banco de dados");
+    app.listen(port, () => {
+      console.log(`Servidor rodando na porta ${port}`);
+    });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.error("Erro de conexão com o MongoDB", err));
